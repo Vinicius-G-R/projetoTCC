ad5ea9c3682cc9ff44475cfbea4def3060d19be9
vinicius-g-r <viniciussouza26032005@gmail.com>
2023-10-26 14:17:36 -0300

    feito

---
+++ js/scripPage.js
@@ -0,0 +1,11 @@
+/*let iniciar = () => {
+    let textoT = document.getElementById("textoT");
+    let mostrarReferencia = () =>{
+        let textoR = getElementById("textoR");
+        textoR.style.display = 'hidden';
+    } 
+
+textoT.addEventListener("click", mostrarReferencia)
+};
+document.addEventListener('DOMContentLoaded', iniciar);
+*/
